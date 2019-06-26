a4be00dd7fe7cedf051acf8f2316a3a91fc9089d
TareQ <Tareq.mani@outlook.com>
2019-06-26 12:10:25 +0300

    psuhing new classes

--- js/set.js
+++ js/set.js
@@ -77,9 +77,9 @@ class Shape extends React.Component {
     render() {
         return (
              <div className="shape">
-                 <div className="circle dotted red "></div>
-                 <div className="square dashed blue "></div> 
-                 <div className="oval solid green"></div>
+                 <div className="circle empty red "></div>
+                 <div className="square lined blue "></div> 
+                 <div className="triangle full green"></div>
              </div>
         );
     }
